61308d9e0e70509aac50c2c0a6dfae1ca86be458
AlreadyRich <50845506+kolyapvp@users.noreply.github.com>
2024-12-03 11:50:15 +0300

    Add files via upload

---
+++ shortLink/internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	TimeOut     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"1s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH=./config/local.yaml")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("CONFIG_PATH does not exist")
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal(err)
+	}
+	return &cfg
+}
